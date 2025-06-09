707a274b1565e4e9ba40cb200ebbb8f5184ae584
hadiyasameer <hadiyasameer2013@gmail.com>
2025-06-09 22:09:27 +0000

    updated card image size in mobile view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <footer className="bg-yellow-500 text-white text-center py-3 mt-auto">
-            <p>&copy; 2024 Skip Hire. <a href="#">All rights reserved.</a></p>
-        </footer>
-    )
+  return (
+    <footer className="bg-yellow-500 text-white text-center py-3 mt-auto">
+      <p>
+        &copy; 2025 Skip Hire. <a href="#">All rights reserved.</a>
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
